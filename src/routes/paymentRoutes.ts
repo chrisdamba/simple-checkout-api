06ae6f436a113e50f2fde92e1660b86060eb1ba9
Christopher Dambamuromo <chridam@gmail.com>
2024-09-19 13:14:16 +0100

    chore: implement routes

---
+++ src/routes/paymentRoutes.ts
@@ -0,0 +1,22 @@
+import {Router} from 'express'
+import {PaymentController} from '../controllers/PaymentController'
+
+const router = Router()
+const paymentController = new PaymentController()
+
+router.post('/', paymentController.createPayment.bind(paymentController))
+router.put(
+  '/:id/status',
+  paymentController.updatePaymentStatus.bind(paymentController)
+)
+router.get('/', paymentController.getAllPayments.bind(paymentController))
+router.get(
+  '/status',
+  paymentController.getPaymentsByStatus.bind(paymentController)
+)
+router.get(
+  '/total-completed',
+  paymentController.getTotalCompletedPayments.bind(paymentController)
+)
+
+export {router as paymentRoutes}
